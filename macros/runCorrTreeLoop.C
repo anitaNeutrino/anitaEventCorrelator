6ec4cb0e2f3eaac6aca7a753dea4a7c258928ceb
swb <swb@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2009-01-27 11:01:34 +0000

    mac friendly version

--- macros/runCorrTreeLoop.C
+++ macros/runCorrTreeLoop.C
@@ -2,19 +2,24 @@
 
 void runCorrTreeLoop() {
   //  gSystem->AddIncludePath(gSystem->ExpandPathName("-I${EVENT_READER_DIR}"));
-  gSystem->AddIncludePath("-I${EVENT_READER_DIR}");
-  gSystem->AddIncludePath("-I${PLOTTER_DIR}");
+  //gSystem->AddIncludePath("-I${EVENT_READER_DIR}");
+  //gSystem->AddIncludePath("-I${PLOTTER_DIR}");
   //  cout << gSystem->GetIncludePath() <<endl;
 			  
   gSystem->Load("libMathMore.so");
   gSystem->Load("libPhysics.so");
-  gSystem->Load("/usr/lib64/libfftw3.so");
-  //  gSystem->Load("/unix/anita/softwareSLC4/install/lib/libfftw3.so");
-  gSystem->Load("libAnitaEvent.so");
-  gSystem->Load("libAnitaCorrelator.so");
+  // gSystem->Load("/usr/lib64/libfftw3.so");
+    gSystem->Load("/sw/lib/libfftw3.so");
+     gSystem->Load("../../lib/libRootFftwWrapper.so");
+
+    gSystem->Load("/Applications/root/lib/libMinuit.so");
+  gSystem->Load("../../lib/libAnitaEvent.so");
+  gSystem->Load("../../lib/libAnitaCorrelator.so");
   gSystem->CompileMacro("correlationTreeLoop.C","k");
-  correlationTreeLoop(1027);
-  correlationTreeLoop(1028);
-  //   plotPrettyThings(1028,146380,16); //Run,entry,antenna
+   correlationTreeLoop(18);
+  //  correlationTreeLoop(1028);
+ 
+
+ //   plotPrettyThings(1028,146380,16); //Run,entry,antenna
    //  plotPrettyThings(1028,33995,28);
 }
