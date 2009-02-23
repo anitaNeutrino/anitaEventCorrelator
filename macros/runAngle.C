f4550f40b028c282f412b3aa89467cab604eb549
swb <swb@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2009-02-23 11:49:50 +0000

    updated

---
+++ macros/runAngle.C
@@ -0,0 +1,21 @@
+void runAngle() {
+  gSystem->AddIncludePath("-I${ANITA_UTIL_INSTALL_DIR}/include");
+
+  //Load libraries. Need to have ANITA_UTIL_INSTALL_DIR/lib and ROOTSYS/lib in the LD_LIBRARY_PATH               
+  gSystem->Load("libfftw3.so");
+  gSystem->Load("libMathMore.so");
+  gSystem->Load("libPhysics.so");  
+  gSystem->Load("libGeom.so");  
+  gSystem->Load("libMinuit.so");  
+  gSystem->Load("libRootFftwWrapper.so");         
+  gSystem->Load("libAnitaEvent.so");      
+  gSystem->Load("libAnitaCorrelator.so");
+
+
+  gSystem->CompileMacro("anglePlotter.C","k");
+    anglePlotter();
+
+  // gSystem->CompileMacro("anglePlotterOpt.C","k");
+  //   anglePlotterOpt();
+
+}
