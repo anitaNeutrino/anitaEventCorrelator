8d3e41ab4c8405bd3e87d3a6dda3d08e17665a4f
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-10-16 14:17:56 -0500

    update /add macros

--- macros/testInversion.C
+++ macros/testInversion.C
@@ -1,9 +1,16 @@
 #include "RefractionModel.h" 
 
-void testRefraction() 
+void testInversion(double hmin = 0, double hmax = 200, double payload=35e3, double A =0.1) 
 {
   
   const AntarcticAtmosphere::AtmosphericModel & m = AntarcticAtmosphere::ITURefractivity(); 
+  const AntarcticAtmosphere::ArtificialInversion & im = AntarcticAtmosphere::ArtificialInversion(m, hmax, A); 
+  TCanvas * c = new TCanvas; 
+
+
+  c->Divide(2,1); 
+  c->cd(1); 
+  im.makeGraph(hmin,payload, 1000, AntarcticAtmosphere::REFRACTIVITY)->Draw("alp"); 
 
   Refraction::RaytracerSpherical ray(&m); 
 
@@ -11,18 +18,17 @@ void testRefraction()
   Refraction::RaytracerSpherical::Setup * s =  new Refraction::RaytracerSpherical::Setup;; 
   Refraction::RaytracerSpherical::Result * r  = new Refraction::RaytracerSpherical::Result; 
 
-  TFile f("raytrace.root","RECREATE"); 
+  TFile f("inversion.root","RECREATE"); 
   TTree * t = new TTree("raytrace","Raytrace"); 
   t->Branch("setup",&s); 
   t->Branch("result",&r); 
+  c->cd(2); 
 
-  for (double h = 0; h <= 4000; h+= 500)
+  double h = hmin; 
+  double H = payload; 
+  double step = 0.0005; 
+  for (double el = 0.0005; el <= 89; el+= step)
   {
-    for (double H = 35e3; H <= 40e3; H+= 500)
-    {
-      double step = 0.0005; 
-      for (double el = 0.0005; el <= 89; el+= step)
-      {
         if (el >= 0.1) step= 0.005; 
         if (el >= 1)  step = 0.05;
 
@@ -33,11 +39,9 @@ void testRefraction()
         t->Fill(); 
 
 
-        if (h == 0 && H == 40e3 && el == 0.0005) ray.draw(); 
+        if (el == 0.0005) ray.draw(); 
 
 
-      }
-    }
   }
 
   t->Write(); 
