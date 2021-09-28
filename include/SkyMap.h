4ed8f73b9627d41cc88af8e15c5d115575b3d2f9
Cosmin Deaconu <cozzyd@gmail.com>
2021-09-27 21:24:06 -0500

    update defaults

--- include/SkyMap.h
+++ include/SkyMap.h
@@ -7,6 +7,7 @@
 #include "TMarker.h" 
 #include "TGraph.h" 
 #include "TText.h" 
+#include "TLatex.h" 
 
 /* My attempt at making a sky map. It supports: 
  *   
@@ -25,16 +26,24 @@ class SkyMap : public TObject
     SkyMap(double lon_0 = 180, int nbinsx = 1000, int nbinsy = 1000, const TH2 * background = 0, const std::vector<const TMarker *> *  markers = 0, 
                                    const std::vector<const TGraph*>  * graphs = 0); 
 
+    SkyMap(const SkyMap & other); 
+
     virtual void Paint(Option_t * option = ""); 
     void setTitle(const char * title) { sky_background.SetTitle(title) ; } 
     void setReverseX(bool r) { reverseX = r; } 
-    void setBackground(const TH2 * background); 
+    void setBackground(const TH2 * background, bool ra_in_hours=false); 
     void addMarker(const TMarker * marker); 
-    void addGraph(const TGraph * graph); 
+    void addGraph(const TGraph * graph, bool ra_in_hours=false); 
+    void addText(const TLatex * txt); 
     void clearMarkers() { sky_markers.clear() ; }
+    void setMinimum(double min) { sky_background.SetMinimum(min); }
+    void setMaximum(double max) { sky_background.SetMaximum(max); }
+    TH2 & getBackground() { return sky_background ; } 
     void clearGraphs() { sky_graphs.clear() ; } 
     void ExecuteEvent(Int_t event, Int_t px, Int_t py) { sky_background.ExecuteEvent(event,px,py); } 
 
+    void setRange(double min_ra, double max_ra, double min_dec, double max_dec); 
+    void unsetRange() { range_set = false; }
     virtual ~SkyMap(); 
 
     static void toMollweide(double lon, double lat, double &x, double &y,double lon_0 = 0,  bool reverse_x = true); 
@@ -44,9 +53,12 @@ class SkyMap : public TObject
     
     bool reverseX; 
     double lon_0; 
+    bool range_set; 
+    double range[4]; 
     TH2D sky_background; 
     std::vector<TMarker> sky_markers; 
     std::vector<TGraph> sky_graphs; 
+    std::vector<TLatex> sky_text; 
     TText left; 
     TText right; 
     TPaletteAxis * zaxis; 
--- src/SkyMap.cxx
+++ src/SkyMap.cxx
@@ -29,6 +29,7 @@ static void paintSpecials(bool reverse = true)
     TEllipse * ell =  new TEllipse(0,0,2,1); 
     curves.push_back(ell); 
     ell->SetLineColor(16); 
+    ell->SetLineWidth(3); 
     ell->SetFillStyle(0); 
 
 
@@ -40,6 +41,7 @@ static void paintSpecials(bool reverse = true)
       glat->SetPoint(1,0,ilat); 
       glat->SetPoint(2,179.9999,ilat); 
       glat->SetLineColor(16); 
+      glat->SetLineWidth(2); 
       convert(glat); 
       curves.push_back(glat); 
     }
@@ -52,6 +54,7 @@ static void paintSpecials(bool reverse = true)
       for (int i = 0; i <= 180; i++) glon->SetPoint(i, ilong, i-90); 
       convert(glon); 
       glon->SetLineColor(16); 
+      glon->SetLineWidth(2); 
       curves.push_back(glon); 
     }
   }
@@ -63,12 +66,15 @@ static void paintSpecials(bool reverse = true)
 
 
 
+
+static int skymap_counter = 0; 
 SkyMap::SkyMap(double lon_0, int nbinsx , int nbinsy , const TH2 * background, const std::vector<const TMarker * > * markers, const std::vector<const TGraph *> * graphs )
-  : reverseX(true), lon_0(lon_0), sky_background("skymap_hist", "Sky Map", nbinsx, -2, 2., nbinsy*1.1, -1, 1), 
+  : reverseX(true), lon_0(lon_0), sky_background(Form("skymap_hist_%d", skymap_counter++),  "Sky Map", nbinsx, -2, 2., nbinsy*1.1, -1, 1), 
     left(-2.1,0, TString::Format("%g",reverseX ? lon_0 + 180: lon_0-180)), 
     right(2.1,0, TString::Format("%g",reverseX?lon_0 -180:lon_0+180)) 
 {
   zaxis = 0; 
+  range_set = false; 
   sky_background.SetDirectory(0); 
   left.SetTextAlign(32); 
   right.SetTextAlign(12); 
@@ -90,20 +96,19 @@ SkyMap::SkyMap(double lon_0, int nbinsx , int nbinsy , const TH2 * background, c
 }
 
 
-static double aux_angle(double lat_rad, double eps = 1e-12, int maxiter = 100) 
+static double aux_angle(double lat_rad, double eps = 1e-12, int maxiter = 1000) 
 {
-  if (cos(TMath::Pi()/2) < 0.0001) return lat_rad; 
+  if (cos(lat_rad) < 0.0001) return lat_rad; 
 
-  double theta = lat_rad; 
+  double two_theta = lat_rad; 
   int niter = 0; 
   while(true) 
   {
-    double theta_next = theta - (2 * theta + sin(2*theta) - TMath::Pi() * sin(lat_rad)) /  (2 + 2 * cos(2*theta)); 
+    double d_two_theta = -( two_theta + sin (two_theta) - TMath::Pi() * sin(lat_rad)) / (1 + cos(two_theta));  
 
-    double dtheta = theta_next - theta; 
-    if (fabs(dtheta) < eps || niter >= maxiter) return theta_next; 
+    two_theta += d_two_theta; 
+    if (niter > 5 && (fabs(d_two_theta) < eps || niter >= maxiter)) return two_theta/2; 
     niter++; 
-    theta = theta_next; 
   }
 }
 
@@ -121,14 +126,14 @@ void SkyMap::fromMollweide(double x, double y, double &lon, double & lat, double
 {
   if (revx) x = -x; 
   double theta = asin(y); 
-  double lat_rad = asin((2*theta+sin(2*theta))/TMath::Pi()); 
+  double lat_rad = asin(  (2*theta+sin(2*theta))/TMath::Pi()  ); 
   double lon_rad = TMath::Pi() * x / (2 * cos(theta)); 
   lat = TMath::RadToDeg()*lat_rad; 
   lon = FFTtools::wrap(TMath::RadToDeg()*lon_rad + lon_0,360,0); 
 }
 
 
-void SkyMap::setBackground(const TH2 * bg) 
+void SkyMap::setBackground(const TH2 * bg, bool ra_in_hours) 
 {
   if (!bg) 
   {
@@ -137,6 +142,7 @@ void SkyMap::setBackground(const TH2 * bg)
     zaxis = 0; 
   }
 
+  double scale = 1; 
 
   double lat,lon; 
   for (int i = 1; i <= sky_background.GetNbinsX(); i++) 
@@ -147,6 +153,7 @@ void SkyMap::setBackground(const TH2 * bg)
       double y = sky_background.GetYaxis()->GetBinCenter(j); 
       if ((x*x)/4 + y*y >1) continue; 
       fromMollweide(x,y,lon,lat,lon_0, reverseX); 
+      if (ra_in_hours) lon/=15; 
 
       //convert latitude/lon to histogram units 
       lon = FFTtools::wrap(lon, bg->GetXaxis()->GetXmax() - bg->GetXaxis()->GetXmin(), 0.5 * (bg->GetXaxis()->GetXmin() + bg->GetXaxis()->GetXmax())); 
@@ -166,11 +173,21 @@ void SkyMap::addMarker(const TMarker * marker)
   sky_markers[sky_markers.size()-1].SetY(y); 
 }
 
+void SkyMap::addText(const TLatex * text) 
+{
+  double x,y; 
+  toMollweide(text->GetX(), text->GetY(),x,y, lon_0, reverseX);; 
+  sky_text.push_back(TLatex(*text)); 
+  sky_text.back().SetX(x); 
+  sky_text.back().SetY(y); 
+}
 
-void SkyMap::addGraph(const TGraph * gr) 
+void SkyMap::addGraph(const TGraph * gr, bool ra_in_hours) 
 {
   sky_graphs.push_back(TGraph(gr->GetN())); 
   TGraph & g = sky_graphs[sky_graphs.size()-1]; 
+  double xscale = 1; 
+  if (ra_in_hours) xscale = 15; 
 
   g.SetLineStyle(gr->GetLineStyle()); 
   g.SetLineColor(gr->GetLineColor()); 
@@ -178,15 +195,35 @@ void SkyMap::addGraph(const TGraph * gr)
 
   for (int i = 0; i < gr->GetN(); i++) 
   {
-    toMollweide(gr->GetX()[i], gr->GetY()[i], g.GetX()[i], g.GetY()[i],lon_0, reverseX); 
+    toMollweide(gr->GetX()[i]*xscale, gr->GetY()[i], g.GetX()[i], g.GetY()[i],lon_0, reverseX); 
   }
 }
 
 
 void SkyMap::Paint(Option_t * opt) 
 {
-  gPad->SetFrameLineColor(0); 
-  gPad->SetFrameLineWidth(0); 
+
+  if (range_set) 
+  {
+    double xmin, xmax, ymin,ymax; 
+
+    toMollweide(range[1],range[3],xmax,ymax); 
+    toMollweide(range[0],range[2],xmin,ymin); 
+
+    if (xmax < xmin) std::swap(xmax,xmin); 
+
+    sky_background.GetXaxis()->SetRangeUser(xmin,xmax);
+    sky_background.GetYaxis()->SetRangeUser(ymin,ymax);
+  }
+  else
+  {
+
+    sky_background.GetXaxis()->UnZoom();
+    sky_background.GetYaxis()->UnZoom(); 
+    gPad->SetFrameLineColor(0); 
+    gPad->SetFrameLineWidth(0); 
+  }
+
 
   sky_background.Paint("COL A"); 
 
@@ -194,22 +231,54 @@ void SkyMap::Paint(Option_t * opt)
 
   for (unsigned i = 0; i < sky_graphs.size(); i++) sky_graphs[i].Paint("lsame"); 
   for (unsigned i = 0; i < sky_markers.size(); i++) sky_markers[i].Paint("same"); 
-
+  for (unsigned i = 0; i < sky_text.size(); i++) sky_text[i].Paint("same"); 
   left.Paint(); 
   right.Paint(); 
 
-  if (zaxis) zaxis->Paint(); 
-
+  if (zaxis && strcasestr(opt,"z") ) zaxis->Paint(); 
 
 }
 
 
+void SkyMap::setRange(double min_ra, double max_ra, double min_dec, double max_dec) 
+{
+  range_set = true; 
+  range[0] = min_ra; 
+  range[1] = max_ra; 
+  range[2] = min_dec; 
+  range[3] = max_dec; 
+}
 
 
 SkyMap::~SkyMap() 
 {
-
   if (zaxis) delete zaxis; 
 }
 
 
+SkyMap::SkyMap(const SkyMap & other) 
+  : TObject(other)
+{
+  reverseX = other.reverseX; 
+  lon_0 = other.lon_0; 
+  range_set = other.range_set; 
+  if (range_set) 
+  {
+    std::copy(other.range, other.range+4, range); 
+  }
+
+  sky_background = other.sky_background; 
+  sky_markers = other.sky_markers;
+  sky_graphs = other.sky_graphs;
+  for (const TLatex & ltx : other.sky_text)
+  {
+    sky_text.push_back(TLatex(ltx)); 
+  }
+  left = other.left; 
+  right = other.right;
+  if (other.zaxis) 
+  {
+    zaxis = new TPaletteAxis(*other.zaxis); 
+  }
+  else zaxis = 0; 
+}
